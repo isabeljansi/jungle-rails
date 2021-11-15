2da995584c32e6b6c209986465eec13b974649c1
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-11-14 20:52:49 -0500

    add admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  root to: 'products#index'
-
   resources :about, only: [:index]
 
+  root to: 'products#index'
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -17,8 +15,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
